1044e91594c0f667523004ed0567755363f21dc4
ocramh <ocramh11@gmail.com>
2020-10-29 08:51:57 +0000

    define RSA cryptor type for managing symmetric encryption/decryption ops

---
+++ src/rsa/cryptor.rs
@@ -0,0 +1,34 @@
+extern crate openssl;
+
+use super::keygen::{BoxResult, KeyGen, KeyPair};
+
+pub struct Cryptor {
+  keys: KeyPair,
+}
+
+impl Cryptor {
+  pub fn new(keygen: &KeyGen) -> BoxResult<Self> {
+    let keypair = keygen.new_keypair()?;
+
+    Ok(Cryptor { keys: keypair })
+  }
+
+  pub fn new_with_keys(pubk: Vec<u8>, privk: Vec<u8>) -> Self {
+    Cryptor {
+      keys: KeyPair {
+        publkey: pubk,
+        privkey: privk,
+      },
+    }
+  }
+
+  pub fn encrypt(&self, content: &[u8]) -> BoxResult<Vec<u8>> {
+    println!("RSA encryption happens here");
+    Ok(Vec::new())
+  }
+
+  pub fn decrypt(&self, content: &[u8]) -> BoxResult<Vec<u8>> {
+    println!("RSA decryption happens here");
+    Ok(Vec::new())
+  }
+}
--- src/rsa/keygen.rs
+++ src/rsa/keygen.rs
@@ -1,27 +1,35 @@
 extern crate openssl;
 
+use openssl::rsa::Rsa;
 use std::error::Error;
-use openssl::rsa::{Rsa};
+use std::str;
 
 const KEY_SIZE: u32 = 4096;
- 
+
 pub type BoxResult<T> = Result<T, Box<dyn Error>>;
 
-pub struct KeyManager {
-    pub publkey_path: String,
-    pub privkey_path: String,
+pub struct KeyGen {}
+
+pub struct KeyPair {
+  pub publkey: Vec<u8>,
+  pub privkey: Vec<u8>,
 }
 
-impl KeyManager {
-    pub fn new_keypair(&self) -> BoxResult<()> {
-        let rsa = Rsa::generate(KEY_SIZE).unwrap();
+impl KeyGen {
+  pub fn new_keypair(&self) -> BoxResult<KeyPair> {
+    let rsa = Rsa::generate(KEY_SIZE).unwrap();
 
-        let priv_key: Vec<u8> = rsa.private_key_to_pem().unwrap(); 
-        let publ_key: Vec<u8> = rsa.public_key_to_pem().unwrap(); 
-    
-        println!("private k: {}", String::from_utf8(priv_key).unwrap());
-        println!("public k: {}", String::from_utf8(publ_key).unwrap());
-    
-        Ok(())
-    }
+    let priv_key: Vec<u8> = rsa.private_key_to_pem().unwrap();
+    let publ_key: Vec<u8> = rsa.public_key_to_pem().unwrap();
+
+    println!("private k: {}", str::from_utf8(&priv_key).unwrap());
+    println!("public k: {}", str::from_utf8(&publ_key).unwrap());
+
+    Ok(KeyPair {
+      publkey: publ_key,
+      privkey: priv_key,
+    })
+  }
+
+  pub fn save_keys_to_file(&self, key_name: String) {}
 }
