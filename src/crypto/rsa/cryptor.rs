a2c16a4345aa60556ebf724f73485fe577450e34
ocramh <ocramh11@gmail.com>
2020-11-07 11:31:32 +0000

    refactor rsa mod to use rsa<Private> as key type rather than bytes

--- src/crypto/rsa/cryptor.rs
+++ src/crypto/rsa/cryptor.rs
@@ -12,33 +12,38 @@ impl Cryptor {
   pub fn new(keygen: &KeyGen) -> BoxResult<Self> {
     let keypair = keygen.new_keypair()?;
 
+    // keygen.save_keys_to_file(&keypair.rsa, &String::from("id_rsa"))?;
+
     Ok(Cryptor { keys: keypair })
   }
 
   #[allow(dead_code)]
-  pub fn new_with_keys(pubk: Vec<u8>, privk: Vec<u8>) -> Self {
-    Cryptor {
+  pub fn new_with_keys(privk: Vec<u8>) -> BoxResult<Self> {
+    let privk_from_pem = Rsa::private_key_from_pem(&privk)?;
+
+    Ok(Cryptor {
       keys: KeyPair {
-        publkey: pubk,
-        privkey: privk,
+        rsa: privk_from_pem,
       },
-    }
+    })
   }
 
   pub fn encrypt(&self, content: &[u8]) -> BoxResult<Vec<u8>> {
-    let key = Rsa::public_key_from_pem(&self.keys.publkey)?;
-    let mut dest_buffer: Vec<u8> = vec![0; key.size() as usize];
-
-    key.public_encrypt(content, &mut dest_buffer, Padding::PKCS1)?;
+    let mut dest_buffer: Vec<u8> = vec![0; self.keys.rsa.size() as usize];
+    self
+      .keys
+      .rsa
+      .public_encrypt(content, &mut dest_buffer, Padding::PKCS1)?;
 
     Ok(dest_buffer)
   }
 
   pub fn decrypt(&self, content: &[u8]) -> BoxResult<Vec<u8>> {
-    let key = Rsa::private_key_from_pem(&self.keys.privkey)?;
-    let mut dest_buffer: Vec<u8> = vec![0; key.size() as usize];
-
-    key.private_decrypt(content, &mut dest_buffer, Padding::PKCS1)?;
+    let mut dest_buffer: Vec<u8> = vec![0; self.keys.rsa.size() as usize];
+    self
+      .keys
+      .rsa
+      .private_decrypt(content, &mut dest_buffer, Padding::PKCS1)?;
 
     println!("Decrypted: {}", str::from_utf8(&dest_buffer).unwrap());
 
