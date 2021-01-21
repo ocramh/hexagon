61f5084b2cb6d91af5ce945403f605a8fed3d678
ocramh <ocramh11@gmail.com>
2021-01-21 09:46:34 +0000

    tidy up crypyo rsa module

--- src/crypto/errors.rs
+++ src/crypto/errors.rs
@@ -1,11 +1,18 @@
+use openssl::error::ErrorStack;
 use thiserror::Error;
 
 #[derive(Debug, Error, PartialEq)]
-pub enum EncrytpError {
+pub enum CryptoError {
   #[error("data encryption failed: {0}")]
   #[allow(dead_code)]
   Encryption(String),
 
   #[error("data decryption failed: {0}")]
   Decryption(String),
+}
+
+impl From<ErrorStack> for CryptoError {
+  fn from(e: ErrorStack) -> Self {
+    CryptoError::Encryption(e.to_string())
+  }
 }
