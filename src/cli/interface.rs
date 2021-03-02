bc5a99db68e3888474c071d82cf633aba5b957e9
ocramh <ocramh11@gmail.com>
2021-03-02 08:29:26 +0000

    update cli args structure

--- src/cli/interface.rs
+++ src/cli/interface.rs
@@ -4,11 +4,25 @@ pub fn cli() {
   let yaml = load_yaml!("cli.yaml");
   let matches = App::from(yaml).get_matches();
 
+  if let Some(ref _matches) = matches.subcommand_matches("symmetric") {
+    println!("do symmetric encryption")
+  }
+
+  if let Some(ref _matches) = matches.subcommand_matches("asymmetric") {
+    println!("do asymmetric encryption");
+  }
+
   if let Some(ref matches) = matches.subcommand_matches("test") {
     if matches.is_present("debug") {
       println!("Printing debug info...");
-    } else {
-      println!("Printing debug info...");
+    }
+
+    if matches.is_present("symmetric") {
+      println!("Run symmetric crypto ops");
+    }
+
+    if matches.is_present("asymmetric") {
+      println!("Run asymmetric crypto ops")
     }
   }
 }
